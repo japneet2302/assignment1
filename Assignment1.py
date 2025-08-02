ff014d9937a7a2c3ad2cea3c24380ee2e7f1ea9b
japneet2302 <107108996+japneet2302@users.noreply.github.com>
2025-08-02 22:27:30 +0530

    Add files via upload

---
+++ Assignment1.py
@@ -0,0 +1,51 @@
+
+#Task1: Perform Basic Mathematical Operations
+'''
+a=input('Enter the first number: ')
+a=int(a)
+b=input('Enter the second number: ')
+b=int(b)
+c= a+b
+print("Addition Result: ",c)
+'''
+
+
+
+'''
+a=input('Enter the first number: ')
+a=int(a)
+b=input('Enter the second number: ')
+b=int(b)
+c= a-b
+print("Subtraction Result: ",c)
+'''
+
+'''
+a=input('Enter the first number: ')
+a=int(a)
+b=input('Enter the second number: ')
+b=int(b)
+c= a*b
+print("Multiplication Result: ",c)
+'''
+
+
+'''
+a=input('Enter the first number: ')
+a=float(a)
+b=input('Enter the second number: ')
+b=float(b)
+c= a/b
+print("Division Result: ",c)
+'''
+
+
+
+#Task2: Create a Personalized Greeting
+a=input('Enter your first name: ')
+b=input('Enter your last name: ')
+a=str(a)
+b=str(b)
+c=a + " "+ b
+d="Hello,"
+print(d, c, "Welcome to the Python program.")
